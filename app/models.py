8fcf766001c6e79d39aec92865f53c75c0a3f54d
akshayverma <akshayvermadtu@gmail.com>
2018-01-11 22:11:28 +0530

    11/1/18 commit

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ import jsonfield
 
 class User(models.Model):
     name = models.CharField(max_length=20)
-    phone = models.PositiveIntegerField(null=True)
+    phone = models.BigIntegerField(null=True)
     address = models.TextField()
     password = models.CharField(max_length=10)
     cart = jsonfield.JSONField(default={})
